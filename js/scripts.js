38e5992b88e01e361313e264ce97a39b45012297
Eusebie Siebenberg <siebenee@gmail.com>
2023-03-20 16:20:17 -0700

    Change alert return to include user input and results in scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -17,10 +17,14 @@ function divide (number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert("The addition of your numbers equals: " + add(number1, number2) + ". " +
-   "The subtraction of your numbers equals: " + subtract(number1, number2) + ". " +
-   "The multiplication of your numbers equals: " + multiply(number1, number2) + ". " +
-   "The division of your numbers equals: " + divide(number1, number2));
+window.alert("The sum of " + number1 + " and " + number2 + " equals: " 
+  + add(number1, number2) 
+  + ". " + "The subtraction of " + number1 + " and " + number2 + " equals: "
+  + subtract(number1, number2)
+  + ". " + "The multiplication of " + number1 + " and " + number2 + " equals: " 
+  + multiply(number1, number2)
+  + ". " + "The division of " + number1 + " and " + number2 + " equals: "
+  + divide(number1, number2));
   
 
   
