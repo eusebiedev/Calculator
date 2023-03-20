c2d32965959d9a7656e2bb775e6e1ef95c7ced7d
Eusebie Siebenberg <siebenee@gmail.com>
2023-03-20 16:05:15 -0700

    Combine 4 separate window.alert() methods into one call showing all results

--- js/scripts.js
+++ js/scripts.js
@@ -17,7 +17,12 @@ function divide (number1, number2) {
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
-window.alert("The addition of your numbers equals: " + add(number1, number2));
-window.alert("The subtraction of your numbers equals: " + subtract(number1, number2));
-window.alert("The multiplication of your numbers equals: " + multiply(number1, number2));
-window.alert("The division of your numbers equals: " + divide(number1, number2));
+window.alert("The addition of your numbers equals: " + add(number1, number2) + ". " +
+   "The subtraction of your numbers equals: " + subtract(number1, number2) + ". " +
+   "The multiplication of your numbers equals: " + multiply(number1, number2) + ". " +
+   "The division of your numbers equals: " + divide(number1, number2));
+  
+
+  
+  
+ 
